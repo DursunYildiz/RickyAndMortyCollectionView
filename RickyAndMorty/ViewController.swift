72c3a7565c0530d62036ca4a5593f1849a1583f7
dursun yıldız <dursun.yildiz@a101.com.tr>
2022-01-05 23:05:29 +0300

    Initial Commit

---
+++ RickyAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyAndMorty
+//
+//  Created by A101Mac on 5.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
